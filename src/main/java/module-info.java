0a07cbcbba2de5dbf9a6fcc7a671e1e4bca9da36
Verena Blaschke <vebl@protonmail.com>
2022-05-09 13:13:32 +0200

    Make compatible with deployment version

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module de.tuebingen.sfs.psl.gui {
+	exports de.tuebingen.sfs.psl.gui;
+	requires de.tuebingen.sfs.psl;
+	requires psl.core;
+    requires h2;
+    requires java.desktop;
+    requires javafx.base;
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+}
