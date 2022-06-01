46c18c3d5761ada82618948f5e89a9bde904655f
Verena Blaschke <vebl@protonmail.com>
2022-06-01 14:43:45 +0200

    Add license to code

--- src/main/java/de/tuebingen/sfs/psl/gui/AtomVerbalizationRenderer.java
+++ src/main/java/de/tuebingen/sfs/psl/gui/AtomVerbalizationRenderer.java
@@ -1,3 +1,18 @@
+/*
+ *     Copyright 2018–2022 University of Tübingen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.tuebingen.sfs.psl.gui;
 
 import java.util.HashMap;
@@ -10,7 +25,6 @@ import javafx.scene.text.TextFlow;
 
 public class AtomVerbalizationRenderer {
 	public static void fillTextFlow(String verbalization, TextFlow tf, FactWindow fw) {
-		// TODO use value of influencialRule
 		HashMap<Mode, String> modeToStyle = new HashMap<>();
 
 		Stack<State> stack = new Stack<>();
@@ -70,8 +84,6 @@ public class AtomVerbalizationRenderer {
 
 		}
 	}
-
-	;
 
 	private static void createLabel(String entry, Mode mode, Stack<State> stack, TextFlow tf,
 			HashMap<Mode, String> modeToStyle, FactWindow fw) {
